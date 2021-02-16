ad28ea44c3eacb610501a6480016b9396cbb3acb
onurgoz <48811337+onurgoz@users.noreply.github.com>
2021-02-17 01:13:45 +0300

    Brand and Color added. EntityFramwork added. IEntityRepository added.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -14,9 +14,40 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length >= 2)
+            {
+                if (car.DailyPrice > 0)
+                {
+                    _carDal.Add(car);
+                    Console.WriteLine(car.CarName + " added.");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the daily price value greater than 0. \n Daily price : " + car.DailyPrice);
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter the car name value greater than 2 characters. \n Car name : " + car.CarName);
+            }
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,11 +10,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager=new CarManager(new InMemoryCarDal());
+            CarManager carManager=new CarManager(new EfCarDal());
             foreach (var Car in carManager.GetAll())
             {
                 Console.WriteLine(Car.ColorId);
             }
+            
+            carManager.Add(new Car {BrandId=4,ColorId=4,CarName="CarName2",ModelYear=2004,DailyPrice=2000,Description="Description4" });
         }
     }
 }
